4b9ce5d1356a1b011a17e6094a4a6b20efd36ba7
florianmenguy <fmenguy44@gmail.com>
2015-07-21 22:28:24 +0200

    Update rwc_classify.m

--- rwc_classify.m
+++ rwc_classify.m
@@ -1,6 +1,4 @@
 function [predicted_label, true_labels,dureeTotal,testLabelTot,accuracy] = rwc_classify(instrument_features)
-
-[m,~]=size(instrument_features);
 trainSamplesTot=[];
 trainLabel1=[];
 testSamples=[];
@@ -11,47 +9,45 @@ testLabel=[];
 testLabelTot=[];
 testfinal=[];
 dureeTotal=[];
-for i=1:m
-    temp=instrument_features(i,:);
-    idx = cellfun('isempty',temp);
-    temp(idx) = [];
-    [~,c]=size(temp);
-    labels=i;
-    groups = (ceil(ones(c,1)))*labels;
+for w=1:length(instrument_features)
+    nbfiles=length(instrument_features{w})
+    temp=instrument_features{w};
+    
+    
+    [l,~]=size(temp);
+    labels=w;
+    groups = (ceil(ones(l,1)))*labels;
     [train,test] = crossvalind('HoldOut',groups, 0.5);
     %%Train
     trainSamples=temp(train);
-    [~,c]=size(trainSamples);
-    trainSamplesTot=[trainSamplesTot  trainSamples];
+    tailletrainSamples=length(trainSamples);
+    trainSamplesTot=[trainSamplesTot;trainSamples];
     trainLabel=groups(train,1);
     trainLabelTot=[trainLabelTot; trainLabel];
-    for z=1:c
+    for z=1:tailletrainSamples
         [~,r]=size(trainSamples{z});
-        labeltemp=ones(1,r)*i;
+        labeltemp=ones(1,r)*w;
         trainLabel1=[trainLabel1 labeltemp];
     end
     %%Test
     testSamples=temp(test);
-    [~,c]=size(testSamples);
-    testSamplesTot=[testSamplesTot testSamples];
+    tailletestSamples=length(testSamples);
+    testSamplesTot=[testSamplesTot;testSamples];
     testLabel=groups(test,1);
-    testLabelTot=[testLabelTot;testLabel]
-    for o=1:length(testSamples)
-        int=testSamples{o};
-        [~,z]=size(int);
-        dureeTotal=[dureeTotal z];
-    end
-    for z=1:c
+    testLabelTot=[testLabelTot;testLabel];
+    for z=1:tailletestSamples
         [~,r]=size(testSamples{z});
-        labeltemp=ones(1,r)*i;
+        labeltemp=ones(1,r)*w;
         true_labels=[true_labels labeltemp];
+        dureeTotal=[dureeTotal r];
     end
-
+    
 end
-trainSamplesTot=cell2mat(trainSamplesTot);
-testSamplesTot=cell2mat(testSamplesTot);
+
+trainSamplesTot=cell2mat(trainSamplesTot');
+testSamplesTot=cell2mat(testSamplesTot');
 %     %Création du modèle
-    model = svmtrain(trainLabel1', trainSamplesTot' ,'-s 0  -t 2 -c 1 -g 1');
-    [predicted_label, accuracy,prob_estimates] = svmpredict(true_labels', testSamplesTot', model);
-   
-    [A1, V]  = confusionmat(true_labels, predicted_label);
+model = svmtrain(trainLabel1', trainSamplesTot' ,'-s 0  -t 2 -c 1 -g 8');
+[predicted_label, accuracy,prob_estimates] = svmpredict(true_labels', testSamplesTot', model);
+
+[A1, V]  = confusionmat(true_labels', predicted_label);
