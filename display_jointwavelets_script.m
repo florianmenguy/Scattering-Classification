b36874e3351baf5dc3827f22ab35b5992fe209db
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-09 12:06:12 +0200

    finish display wavelets script

--- display_jointwavelets_script.m
+++ display_jointwavelets_script.m
@@ -12,8 +12,10 @@ firstorder_scales = [archs{1}.banks{1}.metas.scale] / fs * 1000;
 [~, max_gamma] = find(firstorder_scales>max_firstorder_context,1);
 opts{1}.time.gamma_bounds = [1 max_gamma];
 
+% Non-linéarité
 opts{1}.nonlinearity.name = 'modulus';
 
+% Second ordre
 opts{2}.time = struct();
 transpinv_octaves = 0.5; % transposition invariance in octaves
 nChromas = opts{1}.time.max_Q;
@@ -21,15 +23,17 @@ transpinv_chromas = round(transpinv_octaves * nChromas);
 opts{2}.gamma.T = 32;
 opts{2}.gamma.U_log2_oversampling = Inf;
 
+archs = sc_setup(opts);
 %%
-U2 = jointwavelets(archs);
+Y2 = jointwavelets(archs);
 
 %%
 j2 = 5;
-beta = 1;
-scaling = 64/max(U2{1}.data{7}{beta}(:));
-strf_asc = scaling * U2{1}.data{j2}{beta}(:,:,1).';
-strf_desc = scaling * U2{1}.data{j2}{beta}(:,:,2).';
+beta = 2;
+strf = real(Y2{end}{1}.data{j2}{beta});
+scaling = 64/max(strf(:));
+strf_asc = scaling * strf(:,:,1).';
+strf_desc = scaling * strf(:,:,2).';
 
 subplot(211);
 imagesc(strf_asc);
