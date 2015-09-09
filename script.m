7d73c82b8ac9390155c9a752fcc85511e18f63c6
florianmenguy <fmenguy44@gmail.com>
2015-09-09 13:24:14 +0200

    Update script.m
    Problème sur la partie transpinv_octaves :Error using min
    Matrix dimensions must agree.
    
    Error in fill_bank_metas (line 11)
            scales = min(unbounded_scales,bank_spec.max_scale);
    
    Error in setup_bank (line 3)
    bank.metas = fill_bank_metas(bank.spec);
    
    Error in sc_setup (line 10)
            architecture.banks{variable_index} = ...
    
    Error in script (line 42)
    archs = sc_setup(opts);

--- script.m
+++ script.m
@@ -6,7 +6,7 @@ rng(0)
 % Bank setup
 fs=44100;
 % Valeurs
-T=2048; % Quantité d'invariance 46ms 
+T=2048; % Quantité d'invariance 46 ms
 N = 4*T;% Chunk de taille 740ms
 
 % Ordre 1 en temps
@@ -14,11 +14,6 @@ opts{1}.time.T = 2048;
 opts{1}.time.max_scale = 4096;%Environ 93 ms
 opts{1}.time.max_Q=8;
 opts{1}.time.size=N;
-
-% max_firstorder_context = 32; % in ms
-% firstorder_scales = [archs{1}.banks{1}.metas.scale] / fs * 1000;
-% [~, max_gamma] = find(firstorder_scales>max_firstorder_context,1);
-% opts{1}.time.gamma_bounds = [1 max_gamma];
 
 
 % Non-linéarité entre les deux ordres
@@ -29,20 +24,14 @@ opts{1}.nonlinearity.denominator = 1e-2;
 %
 
 opts{2}.time = struct();
-transpinv_octaves = 0.5; % transposition invariance in octaves
+transpinv_octaves = (0.5:0.1:4) ; % transposition invariance in octaves
 nChromas = opts{1}.time.max_Q;
 transpinv_chromas = round(transpinv_octaves * nChromas);
 
 opts{2}.gamma.is_U_blurred = false;
 opts{2}.gamma.T = transpinv_chromas;
 
-% % Création architecture
-archs = sc_setup(opts);
-% % display_bank(archs{1}.banks{1});
-% % display_bank(archs{2}.banks{1});
-
-datapath='~/Documents/MATLAB/Stage/rwc/';
+ datapath='~/Documents/MATLAB/Stage/rwc/';
+ 
  instrument_features = rwc_scatter( datapath,opts);
-
-[predicted_label, true_labels,dureeTotal,testLabelTot,accuracy] = rwc_classify(instrument_features);
-[mean_accuracy, confusion_matrix ,matrixDeci] = rwc_evaluate(predicted_label,dureeTotal,testLabelTot);
+archs = sc_setup(opts);
