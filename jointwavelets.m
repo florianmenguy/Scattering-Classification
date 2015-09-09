93439069725b4a9a31d5b570653005f4d19d6b16
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-09 11:48:37 +0200

    bug fix (I was applying first-order twice)

--- jointwavelets.m
+++ jointwavelets.m
@@ -20,6 +20,6 @@ U{1+1} = Y_to_U(Y{1}{end}, archs{1});
 U{1+1}.data{round(end/2)}(end/2) = 1;
 
 % scattering joint (ondelette horizontale puis ondelette verticale)
-Y{2} = U_to_Y(U{1+1}, archs{1});
-U2 = Y_to_U(Y{2}{end}, archs{1});
+Y{2} = U_to_Y(U{1+1}, archs{2});
+U2 = Y_to_U(Y{2}{end}, archs{2});
 end
