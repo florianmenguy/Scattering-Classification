a99bcf20327bb282b37d516974f5e66ed61b55a1
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-09 11:45:20 +0200

    move content of display_joint to display_jointwavelets_script

--- display_jointwavelets_script.m
+++ display_jointwavelets_script.m
@@ -2,3 +2,16 @@
 
 %%
 U2 = jointwavelets(archs);
+
+%%
+j2 = 7;
+beta = 1;
+scaling = 64/max(S{1+2}{1}.data{j2}{beta}(:));
+strf_asc = scaling * S{1+2}{1}.data{j2}{beta}(:,:,1).';
+strf_desc = scaling * S{1+2}{1}.data{j2}{beta}(:,:,2).';
+
+subplot(211);
+image(strf_asc);
+subplot(212);
+image(strf_desc);
+colormap rev_hot;
