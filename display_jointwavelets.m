055764eb1579a5533c5f6d3b2a92fff30d8838b3
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-09 11:38:24 +0200

    U0

--- display_jointwavelets.m
+++ display_jointwavelets.m
@@ -1,5 +1,17 @@
 function display_jointwavelets(archs)
-% on initialise avec du silence
+% Initialisation avec du silence
 N = archs{1}.banks{1}.spec.size;
 signal = zeros(N,1);
+
+% Initialisation de la structure de scattering
+nLayers = length(archs);
+S = cell(1,1+nLayers);
+U = cell(1,1+nLayers);
+Y = cell(1,1+nLayers);
+
+U{1+0} = initialize_variables_auto(size(target_signal));
+U{1+0}.data = signal;
+
+% on calcule le scalogramme U1
+
 end
