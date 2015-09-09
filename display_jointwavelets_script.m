7e1064712d9da68ad48021365701491f66b2004d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-09 11:49:06 +0200

    rename S{1+2} to U2

--- display_jointwavelets_script.m
+++ display_jointwavelets_script.m
@@ -6,9 +6,9 @@ U2 = jointwavelets(archs);
 %%
 j2 = 7;
 beta = 1;
-scaling = 64/max(S{1+2}{1}.data{j2}{beta}(:));
-strf_asc = scaling * S{1+2}{1}.data{j2}{beta}(:,:,1).';
-strf_desc = scaling * S{1+2}{1}.data{j2}{beta}(:,:,2).';
+scaling = 64/max(U2{1}.data{j2}{beta}(:));
+strf_asc = scaling * U2{1}.data{j2}{beta}(:,:,1).';
+strf_desc = scaling * U2{1}.data{j2}{beta}(:,:,2).';
 
 subplot(211);
 image(strf_asc);
