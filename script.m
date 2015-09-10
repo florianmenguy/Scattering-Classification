114740df0cde6d33c94d372e7c48e0408dbafc8d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-10 03:30:28 +0200

    update datapath

--- script.m
+++ script.m
@@ -1,8 +1,3 @@
-% Bank setup
-fs=44100;
-% Valeurs
-T = 2048; % Quantite d'invariance 46 ms
-
 % Ordre 1 en temps
 opts{1}.time.T = 2048;
 opts{1}.time.max_scale = 4096; % Environ 93 ms
@@ -12,18 +7,18 @@ opts{1}.time.max_Q=8;
 opts{1}.nonlinearity.name = 'uniform_log';
 opts{1}.nonlinearity.denominator = 1e-2;
 
-% Ordre 2 en temps
+% Ordre 2 en temps (dimension horizontale)
 opts{2}.time.handle = @morlet_1d;
 
-% Ordre 2 en 
-transpinv_octaves = 4 ; % transposition invariance in octaves
+% Ordre 2 en log-fréquence (dimension verticale)
+transpinv_octaves = 4 ; % invariance maximale par transposition = 4 octaves
 nChromas = opts{1}.time.max_Q;
 transpinv_chromas = round(transpinv_octaves * nChromas);
-
 opts{2}.gamma.is_U_blurred = false;
 opts{2}.gamma.T = transpinv_chromas;
+opts{2}.gamma.gamma_bounds = [3 Inf];
 
- datapath='~/Documents/MATLAB/Stage/rwc/';
- 
- instrument_features = rwc_scatter( datapath,opts);
+datapath='~/datasets/rwc/';
 archs = sc_setup(opts);
+
+
