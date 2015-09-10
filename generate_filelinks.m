d7c9f89f6112ef87c9caa1cc3056e709e6479852
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-10 10:04:33 +0200

    fix HTML generation

--- generate_filelinks.m
+++ generate_filelinks.m
@@ -1,5 +1,9 @@
 function generate_filelinks()
 fileID = fopen('filelinks.html','w');
-fprintf(fileID, '<a href="jointrwc_%1.2d.mat">%1.2d</a>\n', 1:45, 1:45)
+
+for n = 1:45
+    fprintf(fileID, '<a href="jointrwc_%1.2d.mat">%1.2d</a>\n', n, n);
+end
+
 fclose(fileID);
 end
