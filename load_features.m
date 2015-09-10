745bccffce241e016f22049e2558c5ce5b617606
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-10 11:01:29 +0200

    update load_features

--- load_features.m
+++ load_features.m
@@ -1,17 +1,12 @@
-function features = load_features(setting)
-% Generate prefix string
-prefix = setting2prefix(setting);
+function features = load_features(featurespath)
 nBatches = 45;
-batch_features = cell(nBatches,1);
-% Load batches
+batch_features = cell(nBatches, 1);
 for batch_id = 1:nBatches
     disp(['loading batch #', int2str(batch_id)])
     batch_id_str = num2str(batch_id,'%1.2d');
-    file_name = [prefix, batch_id_str];
-    file_path = ['features/', file_name];
+    file_path = [featurespath, '/jointrwc', batch_id_str, '.mat'];
     load(file_path);
     batch_features{batch_id} = batch;
 end
-% Convert cell array to vector
 features = [batch_features{:}];
 end
