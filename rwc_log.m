0c7a0d9fb4e1d3d8170f40ce4b420dcf7b70f792
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-10 11:54:47 +0200

    fix epsilon

--- rwc_log.m
+++ rwc_log.m
@@ -2,6 +2,6 @@ function samples = rwc_log(samples, epsilon)
 nSamples = length(samples);
 for sample_index = 1:nSamples
     sample = samples(sample_index);
-    samples(sample_index).data = log1p(epsilon*max(sample.data,0))
+    samples(sample_index).data = log1p(max(sample.data,0)/epsilon);
 end
 end
